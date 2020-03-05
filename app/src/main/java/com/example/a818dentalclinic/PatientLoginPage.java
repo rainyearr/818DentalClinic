8c8c5a220895021a63eeab4a20352b20738f762e
Jumac <juliusmacabulos26@gmail.com>
2020-03-05 23:58:30 +0800

    register and login na to

--- app/src/main/java/com/example/a818dentalclinic/PatientLoginPage.java
+++ app/src/main/java/com/example/a818dentalclinic/PatientLoginPage.java
@@ -1,18 +1,33 @@
 package com.example.a818dentalclinic;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
 
 public class PatientLoginPage extends AppCompatActivity {
 
+    EditText email, mPassword;
     Button backButton;
     Button loginButton;
     ImageButton registerButton;
+    ProgressBar progressBar;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,17 +38,7 @@ public class PatientLoginPage extends AppCompatActivity {
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(PatientLoginPage.this , MainActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        loginButton = findViewById(R.id.patient_login);
-
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(PatientLoginPage.this , DoctorAppointments.class);
+                Intent intent = new Intent(PatientLoginPage.this, MainActivity.class);
                 startActivity(intent);
             }
         });
@@ -43,8 +48,54 @@ public class PatientLoginPage extends AppCompatActivity {
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(PatientLoginPage.this , PatientRegister.class);
+                Intent intent = new Intent(PatientLoginPage.this, PatientRegister.class);
                 startActivity(intent);
+            }
+        });
+
+        email = findViewById(R.id.patient_name);
+        mPassword = findViewById(R.id.patient_pass);
+        progressBar = findViewById(R.id.progressBar_patientLogin);
+        fAuth = FirebaseAuth.getInstance();
+        loginButton = findViewById(R.id.patient_login);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                String userName = email.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(userName)) {
+                    email.setError("Username is Required!");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    mPassword.setError("Password is Required!");
+                    return;
+                }
+
+                if (password.length() < 6) {
+                    mPassword.setError("Password must be greater than 5 characters!");
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                fAuth.signInWithEmailAndPassword(userName, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(PatientLoginPage.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), DoctorAppointments.class));
+                        } else {
+                            Toast.makeText(PatientLoginPage.this, "Error ! " + Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
             }
         });
     }
